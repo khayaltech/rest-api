ab500ff88e501f27a1179b0b14c4cedf11518d78
khayaltech <khayaltech@gmail.com>
2022-12-30 02:06:52 +0400

    Spring  security configurations

--- src/main/java/com/example/simpleAPI/SimpleApiApplication.java
+++ src/main/java/com/example/simpleAPI/SimpleApiApplication.java
@@ -3,13 +3,14 @@ package com.example.simpleAPI;
 import com.example.simpleAPI.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 import java.util.List;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SimpleApiApplication {
 
     public static void main(String[] args) {
